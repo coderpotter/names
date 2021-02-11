f24be92319ea750f248de496c01bbbb43b470a3c
coderpotter <anighojkar@usf.edu>
2021-02-11 01:23:29 -0500

    working

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ I'm tired of spelling my name for fast food joints and Walmart pickups :stuck_ou
 This python file reads data from the `NationalNames.csv` file found in this [Kaggle dataset](https://www.kaggle.com/kaggle/us-baby-names?select=NationalNames.csv).
 You can run the file with the following command line inputs:
 1. your name
-2. (optional) gender of your name: default 'F'
-3. (optional) minimum frequency of the suggested name(s) (in 1000s): default 10
-4. (optional) number of suggested names: default 10
+2. (optional) -g: gender of your name --- default 'F'
+3. (optional) -s: Starting letter of the suggested names --- default None
+4. (optional) -l: maximum length of suggested names --- default equal to your name
+5. (optional) -n: number of suggested names --- default 10
 
-Ties will be broken by the commonest name winning and then alphabetically.
+Ties will be broken alphabetically.
--- names.py
+++ names.py
@@ -1,3 +1,35 @@
+import getopt, sys
+from Levenshtein import distance
 import pandas as pd
 
+name = sys.argv[1]
+argumentList = sys.argv[2:]
+options = "g:s:l:n:"
+long_options = ["Gender =", "Starting =", "Length =", "Number ="]
+gender, starting, length, number = None, None, len(name), 10
+
+try:
+    arguments, values = getopt.getopt(argumentList, options)
+    for a, v in arguments:
+        if a in ("-g", "--Gender"):
+            gender = v.upper()
+        elif a in ("-s", "--Starting"):
+            starting = v
+        elif a in ("-l", "--Length"):
+            length = int(v)
+        elif a in ("-n", "--Number"):
+            number = int(v)
+except getopt.error as err:
+    print(str(err))
+
+print('Reading the file')
 names = pd.read_csv("archive/NationalNames.csv")
+names = names[names.Name.str.len() <= length]
+if gender:
+    names = names[names.Gender == gender]
+if starting:
+    names = names[names.Name.startswith(starting[0])]
+
+print('Generating results')
+close_names = sorted(list(set(names.Name.tolist())), key=lambda x: distance(name, x))
+print(close_names[:number])
